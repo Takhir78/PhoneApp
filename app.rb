d7ea0057a4f7af74fdbf3a30919a0aa8c696207a
Takhir78 <t.salimov@gmail.com>
2017-04-05 16:23:13 -0400

    Phinebook PGAdmin4

--- app.rb
+++ app.rb
@@ -14,12 +14,16 @@ get '/' do
 	erb :index, :locals => {:phonebook => phonebook}
 end
 post "/phonebook" do
-first_name = params[:first_name]
-last_name = params[:last_name]
-street_address = params[:street_address]
-city = params[:city]
-zip = params[:zip]
-cell_phone = params[:cell_phone]
-home_phone = params[:home_phone]
-work_phone = params[:work_phone]
+	first_name = params[:first_name]
+	last_name = params[:last_name]
+	street_address = params[:street_address]
+	city = params[:city]
+	state = params[:state]
+	zip = params[:zip]
+	cell_phone = params[:cell_phone]
+	home_phone = params[:home_phone]
+	work_phone = params[:work_phone]
+
+db.exec("INSERT INTO phonebook(first_name, last_name, street_address, city, state, zip, cell_phone, home_phone, work_phone) VALUES('#{first_name}', '#{last_name}', '#{street_address}', '#{city}', '#{state}', '#{zip}', '#{cell_phone}', '#{home_phone}', '#{work_phone}')");
+redirect '/'
 end
